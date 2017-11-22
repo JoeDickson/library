128f99a4678bc1a9f351e6caa13dc4866ca26448
Joe Dickson <dicksonjoe7@gmail.com>
2017-11-21 22:42:24 -0800

    added mongodb

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -2,26 +2,6 @@ var express = require('express');
 var adminRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
 
-var books = [{
-        title: 'Lord of the Rings',
-        genre: 'Fantasy',
-        author: 'Tolkien',
-        read: true
-    },
-    {
-        title: 'War and Peace',
-        genre: 'Historical Ficti0n',
-        author: 'Tolstoy',
-        read: false
-    },
-    {
-        title: 'Mein Kampf',
-        genre: 'Propaganda',
-        author: 'Hitler',
-        read: false
-    }
-];
-
 var router = function(nav) {
 
     adminRouter.route('/addBooks')
