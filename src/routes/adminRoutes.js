ee6057661dcd303772999ab745c0fc7c141a8c09
Joe Dickson <dicksonjoe7@gmail.com>
2017-11-24 19:04:25 -0800

    working db changes

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -2,6 +2,26 @@ var express = require('express');
 var adminRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
 
+var books = [{
+        title: 'Lord of the Rings',
+        genre: 'Fantasy',
+        author: 'Tolkien',
+        read: true
+    },
+    {
+        title: 'War and Peace',
+        genre: 'Historical Ficti0n',
+        author: 'Tolstoy',
+        read: false
+    },
+    {
+        title: 'Mein Kampf',
+        genre: 'Propaganda',
+        author: 'Hitler',
+        read: false
+    }
+];
+
 var router = function(nav) {
 
     adminRouter.route('/addBooks')
@@ -12,7 +32,7 @@ var router = function(nav) {
                 collection.insertMany(books, function(err, results) {
                     res.send(results);
                     db.close();
-                })
+                });
             });
 
             //res.send('inserting books');
