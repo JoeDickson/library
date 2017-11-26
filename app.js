d59c9f50a6d2d50c73f3613bfadb82ee7b658335
Joe Dickson <dicksonjoe7@gmail.com>
2017-11-25 18:25:23 -0800

    Almost added auth

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
+
 var app = express();
-var sql = require('mssql');
-
-var config = {
-    user: 'sa',
-    password: 'cookie',
-    server: 'localhost\\SQLEXPRESS', // You can use 'localhost\\instance' to connect to named instance
-    database: 'Books',
-
-    options: {
-        //encrypt: true // Use this if you're on Windows Azure
-    }
-};
-
-sql.connect(config, function(err) {
-    console.log(err);
-});
 
 var port = process.env.PORT || 5000;
-
 var nav = [{
     Link: '/Books',
     Text: 'Book'
@@ -28,13 +16,23 @@ var nav = [{
 }];
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(session({ secret: 'library' }));
+
+require('./src/config/passport')(app);
+
 app.set('views', './src/views');
+
 app.set('view engine', 'ejs');
 
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
+app.use('/Auth', authRouter);
 
 app.get('/', function(req, res) {
     res.render('index', {
@@ -54,5 +52,5 @@ app.get('/books', function(req, res) {
 });
 
 app.listen(port, function(err) {
-    console.log('App listening on port ' + port);
+    console.log('running server on port ' + port);
 });
